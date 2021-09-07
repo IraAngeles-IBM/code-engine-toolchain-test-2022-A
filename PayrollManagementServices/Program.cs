506af8f472bfd22793d9feabf923e6dfbc69abb2
Isaias Rafael Jr Burao Angeles <isaias@sg.ibm.com>
2021-09-07 13:57:41 +0800

    add userurl in program.cs

--- PayrollManagementServices/Program.cs
+++ PayrollManagementServices/Program.cs
@@ -21,6 +21,7 @@ namespace PayrollManagementService
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:8080");
                 });
     }
 }
