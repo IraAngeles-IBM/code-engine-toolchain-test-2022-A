e78fc586dcc85e78725bfad1450a1bc09681f0d6
Isaias Rafael Jr Burao Angeles <isaias@sg.ibm.com>
2021-09-16 09:51:50 +0800

    create Dockerfile for OpenShift

--- PayrollManagementServices/Program.cs
+++ PayrollManagementServices/Program.cs
@@ -21,7 +21,7 @@ namespace PayrollManagementService
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    // webBuilder.UseUrls("http://*:8080");
+                    webBuilder.UseUrls("http://*:8080");
                 });
     }
 }
